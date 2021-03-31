6c2185772fa9d114ac824e2cc5b36f7eb9b8b4b1
Rahat <rahat3286@gmail.com>
2021-03-31 08:46:45 +0600

    make some changes

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient;
-const cors = require('cors')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
-const port = process.env.PORT || 5000;
-// console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.79vii.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
 
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.79vii.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const port = process.env.PORT || 6060;
+// console.log(process.env.DB_USER)
+
+app.get('/', (req, res) => {
+    res.send("hello from fresh valley it's working")
+
     // console.log(uri);
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
         console.log('connection error', err)
-        const eventCollection = client.db("freshValley").collection("products");
+        const productsCollection = client.db("freshValley").collection("products");
+        const productCollection = client.db("freshValley").collection("products");
         console.log('database connected succefully');
         // perform actions on the collection object
         // client.close();
@@ -25,6 +31,7 @@ app.get('/', (req, res) => {
 
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 6060)
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
