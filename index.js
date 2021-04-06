3e51ca33375c8ade8c3490f0a19bf05b010a299a
Rahat <rahat3286@gmail.com>
2021-04-06 12:13:42 +0600

    make some changes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// working code
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,9 +9,15 @@ require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.79vii.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
+
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello from free valley its working')
+})
+
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const port = 5000;
@@ -23,19 +31,17 @@ client.connect(err => {
     const productsCollection = client.db("freshValleyStore").collection("products");
     const ordersCollection = client.db("freshValleyStore").collection("orders");
 
-    app.post('/addProduct', (req, res) => {
+
+    console.log('database connected')
+
+    app.post("/addProduct", (req, res) => {
         const products = req.body;
         productsCollection.insertOne(products)
             .then(result => {
-                console.log(result.insertedCount);
+                // console.log(result.insertedCount);
                 res.send(result.insertedCount)
             })
     })
-    // const product = {title:" Fresh lentil",description:"lentil",weight:"2 kg",price:100 ,key: "10"}
-    // productsCollection.insertOne(product)
-    // .then(result=>{
-    //     console.log('one product added')
-    // })
 
     app.get('/products', (req, res) => {
         productsCollection.find({}) //.limit(9)
@@ -68,8 +74,106 @@ client.connect(err => {
     })
 });
 
-
 app.listen(port)
+
+// working code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// working code
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const MongoClient = require('mongodb').MongoClient;
+// require('dotenv').config()
+
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.79vii.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// // app.use(bodyParser.urlencoded({ extended:false }));
+
+// const app = express()
+
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// const port = 5000;
+
+
+
+
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//     const productsCollection = client.db("freshValleyStore").collection("products");
+//     const ordersCollection = client.db("freshValleyStore").collection("orders");
+
+//     // app.post('/addProduct', (req, res) => {
+//     //     const products = req.body;
+//     //     console.log(products);
+//     // })
+
+
+//     // working code
+//     app.post('/addProduct', (req, res) => {
+//         const products = req.body;
+//         productsCollection.insertOne(products)
+//             .then(result => {
+//                 console.log(result.insertedCount);
+//                 res.send(result.insertedCount)
+//             })
+//     })
+//     // working code
+
+//     app.get('/products', (req, res) => {
+//         productsCollection.find({}) //.limit(9)
+//             .toArray((err, documents) => {
+//                 res.send(documents);
+//             })
+//     })
+
+//     app.get('/product/:key', (req, res) => {
+//         productsCollection.find({ key: req.params.key }) //.limit(9)
+//             .toArray((err, documents) => {
+//                 res.send(documents[0]);
+//             })
+//     })
+
+//     app.post('/productsByKeys', (req, res) => {
+//         const productKeys = req.body;
+//         productsCollection.find({ key: { $in: productKeys } })
+//             .toArray((err, documents) => {
+//                 res.send(documents);
+//             })
+//     })
+
+//     app.post('/addOrder', (req, res) => {
+//         const order = req.body;
+//         ordersCollection.insertOne(order)
+//             .then(result => {
+//                 res.send(result.insertedCount > 0)
+//             })
+//     })
+// });
+
+
+// app.listen(port)
 
 
 
