bfeb77b6e33010e37228df9b4fcc3d40dd89f0e3
Rahat <rahat3286@gmail.com>
2021-04-07 20:31:15 +0600

    fix some issues

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('hello from freshvalley its working')
+    res.send("hello from BD Grocery it's working")
 })
 
 app.use(bodyParser.json());
